20399c79a0c15c1e9c46e883d624e8000f0d615c
Elias Ruemmler <e.ruemmler@rc-art.de>
2021-02-15 21:11:30 +0100

    Clean up code

--- src/LC_01_Nano/LC_01_Nano.ino
+++ src/LC_01_Nano/LC_01_Nano.ino
@@ -4,7 +4,7 @@
 //-----------------------------------------------------------------------------
 // The MIT License (MIT)
 //
-// Copyright (c) 2021 100prznt.de - info@100prznt.de - February 15th 2021
+// Copyright (c) 2021 ELIAS RUEMMLER - info@100prznt.de - February 15th 2021
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of
 // this software and associated documentation files (the "Software"), to deal in
@@ -31,34 +31,43 @@
 #include <avr/power.h>
 #endif
 
-#define PIN               7   //Pin an welchem die LEDs angeschlossen sind
-#define NUMPIXELS         38  //Summe aller angeschlossenen LEDs
-#define COLPIXELS         19  //Anzahl der Spalten (LEDs welche gleichzeitig angesteuert werden)
+//-----------------------------------------------------------------------------
+// ANFANG - Benutzereinstellungen
+//-----------------------------------------------------------------------------
 
-#define DIMCOLSBEFORE     5  //Anzahl der Spalten welche vor der Position gedimmt werden
-#define DIMCOLSAFTER      3   //Anzahl der Spalten welche nach der Position gedimmt werden
+#define PIN               7   // Pin an welchem die WS2812 angeschlossen sind
 
-bool colMode = false;         //Spaltenmodus aktiviert, hierbei sind die Einzelnen Strips in Spalten angeordnet.
-int delayval = 150;           //Umschaltzeit für jede Spalte
+#define LEDCOUNT          38  // Summe aller angeschlossenen LEDs
+#define COLCOUNT          19  // Anzahl der Spalten (LEDs welche gleichzeitig angesteuert werden)
 
+#define DIMCOLSBEFORE     2   // Anzahl der Spalten welche vor der Position gedimmt werden (Wert muss > 0 sein)
+#define DIMCOLSAFTER      5   // Anzahl der Spalten welche nach der Position gedimmt werden (Wert muss > 0 sein)
 
-//Folgend muss keine Einstellung am Code mehr vorgenommen werden
+bool ColMode = false;         // Spaltenmodus aktivieren, hierbei sind die Einzelnen Strips in Spalten angeordnet.
+int Rpm = 40;                 // "Umdrehungen" je Minute
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+//-----------------------------------------------------------------------------
+// ENDE - Benutzereinstellungen
+//-----------------------------------------------------------------------------
 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(LEDCOUNT, PIN, NEO_GRB + NEO_KHZ800);
 
+int colDelay = 10;
 uint8_t rowPixels = 1;
 uint32_t defaultColor = 0;
 float dimFactorBefore = 0;
 float dimFactorAfter = 0;
 
+
 void setup()
 {
-  rowPixels = NUMPIXELS / COLPIXELS;
+  rowPixels = LEDCOUNT / COLCOUNT;
   dimFactorBefore = 100.0 / DIMCOLSBEFORE ;
   dimFactorAfter = 100.0 / DIMCOLSAFTER;
+  colDelay = 60000.0 / ((float)Rpm * COLCOUNT);
   
   defaultColor = pixels.Color(0xFF,0x60,0x05); //gelb
+  //defaultColor = pixels.Color(0x00,0x00,0xFF); //blau
   
   pixels.begin();
 
@@ -68,7 +77,7 @@ void setup()
 
 void loop()
 {  
-  for (int i = 0; i < COLPIXELS; i++)
+  for (int i = 0; i < COLCOUNT; i++)
   {
     for (int z = 0; z < DIMCOLSBEFORE; z++)
     {
@@ -80,16 +89,17 @@ void loop()
       setColColor(DIMCOLSBEFORE + i + z, dimColor(defaultColor, (uint8_t)(100 - z * dimFactorAfter)));
     }
     pixels.show();
-    delay(delayval);
+    delay(colDelay);
   }
 }
+
 
 void setColColor(uint8_t col, uint32_t color)
 {
   //Künstlicher Overflow per Modulo  0 .. (COLPIXELS - 1)
-  uint8_t realCol = col % COLPIXELS;
+  uint8_t realCol = col % COLCOUNT;
 
-  if (colMode == true)
+  if (ColMode)
   {
     uint8_t firstPixel = realCol * rowPixels;
   
@@ -102,10 +112,11 @@ void setColColor(uint8_t col, uint32_t color)
   {
     for (int i = 0; i < rowPixels; i++)
     {
-      pixels.setPixelColor(realCol + (i * COLPIXELS), color); 
+      pixels.setPixelColor(realCol + (i * COLCOUNT), color); 
     }
   }
 }
+
 
 // brightness: 0 .. 100 (dark .. bright)
 uint32_t dimColor(uint32_t color, uint8_t brightness)
@@ -114,9 +125,10 @@ uint32_t dimColor(uint32_t color, uint8_t brightness)
   return ((uint32_t)((color&0xFF0000)*scale)&0xFF0000) + ((uint32_t)((color&0x00FF00)*scale)&0x00FF00) + ((uint32_t)((color&0x0000FF)*scale)&0x0000FF);
 }
 
+
 void clearAll()
 {
-  for( int i = 0; i<NUMPIXELS; i++)
+  for( int i = 0; i < LEDCOUNT; i++)
   {
     pixels.setPixelColor(i, 0x000000);
     pixels.show();
